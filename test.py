2fbd4bf6ad63840992635e3e7a104cc238109e10
StockTheHouse <53159320+nstockhauser@users.noreply.github.com>
2022-06-27 11:06:49 +0100

    test is a function

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ for i in variable_list:
 
 
 print(variable_list[2])
+
+def test():
+ print("This is a test function that is defined and printing from being called as 
+a function, not using the print function")
+
+
+test()
